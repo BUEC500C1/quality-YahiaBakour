b6ca3cccc2cb2d922419995266a1ccc940d0aed1
Yahia Bakour <39255989+YahiaBakour@users.noreply.github.com>
2020-01-27 17:44:22 -0500

    removed print(e)

--- arabicToRoman.py
+++ arabicToRoman.py
@@ -8,7 +8,6 @@ def arabicToRoman(inputNumber):
         return
     
     if inputNumber < 0 or inputNumber > 3999:
-        print(e)
         return
 
     numToRoman = [(1000,"M"), (900,"CM"), (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), (40,"XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1,"I")]
