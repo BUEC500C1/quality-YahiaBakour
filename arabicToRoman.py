a31819db76ef8f06ce0f5ee8035eece7d32e55ee
Yahia Bakour <39255989+YahiaBakour@users.noreply.github.com>
2020-01-27 17:40:35 -0500

    added code

---
+++ arabicToRoman.py
@@ -0,0 +1,22 @@
+
+
+def arabicToRoman(inputNumber):
+    try:
+        inputNumber = int(inputNumber)
+    except Exception as e:
+        print(e)
+        return
+    
+    if inputNumber < 0 or inputNumber > 3999:
+        print(e)
+        return
+
+    numToRoman = [(1000,"M"), (900,"CM"), (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), (40,"XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1,"I")]
+    answer = ""
+    i = 0
+    while inputNumber:
+        firstdigit = inputNumber // numToRoman[i][0]
+        inputNumber %= numToRoman[i][0]
+        answer += (firstdigit * numToRoman[i][1])
+        i+=1
+    return answer
